11c110ff9f2731b8238f8e1e044888ce1d512f21
iamamartya007 <155907341+iamamartya007@users.noreply.github.com>
2026-02-25 15:59:56 +0530

    Initial commit

---
+++ src/NumAddition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NumAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int a1 = sc.nextInt() ;
+        int a2 = sc.nextInt() ;
+        System.out.println("Sum is  : " +(a1 + a2));
+        int a = 8;
+        int b = 9;
+        int c = 22;
+        int d = a + b + c; // Calculate the sum and store it in d
+        System.out.println(" sum d : " + d); // Print the result
+    }
+}
